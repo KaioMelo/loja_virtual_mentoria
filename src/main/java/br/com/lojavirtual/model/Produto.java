22cabc54cc4f892fb5f8c2f3663da630d87f7ebe
Kaio Melo dos Santos <kaiomelo1998@gmail.com>
2026-05-17 00:47:00 -0300

    commitando outras entidades

--- src/main/java/br/com/lojavirtual/model/Produto.java
+++ src/main/java/br/com/lojavirtual/model/Produto.java
@@ -5,22 +5,12 @@ import java.math.BigDecimal;
 import java.util.Objects;
 
 import javax.persistence.Column;
-import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.ManyToAny;
-
-import br.com.lojavirtual.enums.TipoEndereco;
 
 @Entity
 @Table(name = "produto")
