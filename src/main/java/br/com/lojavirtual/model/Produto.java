4e8f41c8ce79bb2efcf7c5cd1fcba90e8485e52d
Kaio Melo dos Santos <kaiomelo1998@gmail.com>
2026-05-25 20:18:36 -0300

    Atualizando e verificando classes

--- src/main/java/br/com/lojavirtual/model/Produto.java
+++ src/main/java/br/com/lojavirtual/model/Produto.java
@@ -23,25 +23,43 @@ public class Produto implements Serializable{
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_produto")
 	private Long id;
 	
+	@Column(nullable = false)
 	private String tipoUnidade;
+	
+	@Column(nullable = false)
 	private String nome;
+	
+	@Column(nullable = false)
 	private Boolean ativo = Boolean.TRUE;
 	
-	@Column(columnDefinition = "text", length = 2000)
+	@Column(columnDefinition = "text", length = 2000, nullable = false)
 	private String descricao;
 	
 	/** Associar item nota produto**/
 	
+	@Column(nullable = false)
 	private Double peso;
+	
+	@Column(nullable = false)
 	private Double largura;
+	
+	@Column(nullable = false)
 	private Double altura;
+	
+	@Column(nullable = false)
 	private Double profundidade;
+	
+	@Column(nullable = false)
 	private BigDecimal valorVenda = BigDecimal.ZERO;
+	
+	@Column(nullable = false)
 	private Integer qtdEstoque =  0;
+	
 	private Integer qtdAlertaEstoque = 0;
 	private String linkYoutube;
 	private Boolean alertaQtdEstoque = Boolean.FALSE;
 	private Integer qtdClique = 0;
+	
 	public Long getId() {
 		return id;
 	}
--- src/main/java/br/com/lojavirtual/model/VendaCompraLojaVirtual.java
+++ src/main/java/br/com/lojavirtual/model/VendaCompraLojaVirtual.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
@@ -45,17 +46,18 @@ public class VendaCompraLojaVirtual implements Serializable{
 	@ForeignKey(value = ConstraintMode.CONSTRAINT, name = "endereco_cobranca_fk"))
 	private Endereco enderecoCobranca;
 	
+	@Column(nullable = false)
 	private BigDecimal valorTotal;
 	
 	private BigDecimal valorDesconto;
 	
 	@ManyToOne
-	@JoinColumn(name = "forma_pagamento_id", foreignKey = 
+	@JoinColumn(name = "forma_pagamento_id", nullable = false, foreignKey = 
 	@ForeignKey(value = ConstraintMode.CONSTRAINT, name = "forma_pagamento_fk"))
 	private FormaPagamento formaPagamento;
 	
 	@OneToOne
-	@JoinColumn(name = "nota_fiscal_venda_id", foreignKey = 
+	@JoinColumn(name = "nota_fiscal_venda_id",  nullable = false, foreignKey = 
 	@ForeignKey(value = ConstraintMode.CONSTRAINT, name = "nota_fiscal_venda_fk"))
 	private NotaFiscalVenda notaFiscalVenda;
 	
@@ -64,13 +66,17 @@ public class VendaCompraLojaVirtual implements Serializable{
 	@ForeignKey(value = ConstraintMode.CONSTRAINT, name = "cupom_desconto_fk"))
 	private CupomDesconto cupomDesconto;
 	
+	@Column(nullable = false)
 	private BigDecimal valorFrete;
 	
+	@Column(nullable = false)
 	private Integer diaEntrega;
 	
+	@Column(nullable = false)
 	@Temporal(TemporalType.DATE)
 	private Date dataVenda;
 	
+	@Column(nullable = false)
 	@Temporal(TemporalType.DATE)
 	private Date dataEntrega;
 	
