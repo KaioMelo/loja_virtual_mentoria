85157f8f8ac0481d209f00979d796f3b77a4fdd4
Kaio Melo dos Santos <kaiomelo1998@gmail.com>
2026-05-30 10:40:34 -0300

    add primeiros crud e teste

---
+++ src/main/java/br/com/lojavirtual/service/AcessoService.java
@@ -0,0 +1,20 @@
+package br.com.lojavirtual.service;
+
+import org.springframework.stereotype.Service;
+
+import br.com.lojavirtual.model.Acesso;
+import br.com.lojavirtual.repository.AcessoRepository;
+
+@Service
+public class AcessoService {
+
+	private final AcessoRepository acessoRepository;
+	
+	public AcessoService(AcessoRepository acessoRepository) {
+		this.acessoRepository = acessoRepository;
+	}
+	
+	public Acesso save(Acesso acesso) {
+		return acessoRepository.save(acesso);
+	}
+}
